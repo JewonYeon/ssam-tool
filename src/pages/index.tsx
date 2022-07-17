552ab02a99ce5063c513b0cc55144dbdf5ad58b2
jewonyeon <yjw9852@gmail.com>
2022-07-18 02:05:18 +0900

    ⚙️ 초기 세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>ssam-tool</title>
+      </Head>
+
+      <div>Hello World!</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/styles/global-styles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+    min-width: 320px;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  * { font-family: 'Spoqa Han Sans', 'Spoqa Han Sans JP', 'Sans-serif';}
+  a { cursor: pointer; text-decoration: none; }
+`;
